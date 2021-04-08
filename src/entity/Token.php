b0b6ab758b953d21eefdc951d3b2669f2dec3c98
Igor A Tarasov <develop@dicr.org>
2021-04-09 04:20:13 +0500

    add token expireTime

--- src/entity/Token.php
+++ src/entity/Token.php
@@ -3,16 +3,20 @@
  * @copyright 2019-2021 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license MIT
- * @version 07.04.21 05:12:57
+ * @version 09.04.21 04:19:36
  */
 
 declare(strict_types = 1);
 namespace dicr\yandex\oauth\entity;
 
 use dicr\json\JsonEntity;
+use InvalidArgumentException;
 
 /**
  * Токен доступа.
+ *
+ * @property int $createTime timestamp создания токена
+ * @property-read ?int $expireTime timestamp время создания
  */
 class Token extends JsonEntity
 {
@@ -35,6 +39,29 @@ class Token extends JsonEntity
      */
     public $scope;
 
+    /** @var int время создания */
+    private $_createTime;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(array $config = [])
+    {
+        $this->_createTime = time();
+
+        parent::__construct($config);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function attributes(): array
+    {
+        return array_merge(parent::attributes(), [
+            'createTime'
+        ]);
+    }
+
     /**
      * @inheritDoc
      */
@@ -53,5 +80,39 @@ class Token extends JsonEntity
         return [
             'scope' => static fn($val) => empty($val) ? null : explode(' ', (string)$val)
         ];
+    }
+
+    /**
+     * Время создания.
+     *
+     * @return int
+     */
+    public function getCreateTime(): int
+    {
+        return $this->_createTime;
+    }
+
+    /**
+     * Установить время создания.
+     *
+     * @param int $time
+     */
+    public function setCreateTime(int $time): void
+    {
+        if ($time <= 0) {
+            throw new InvalidArgumentException('time: ' . $time);
+        }
+
+        $this->_createTime = $time;
+    }
+
+    /**
+     * Время окончания.
+     *
+     * @return ?int
+     */
+    public function getExpireTime(): ?int
+    {
+        return empty($this->expiresIn) ? null : $this->_createTime + $this->expiresIn;
     }
 }
