a8a01ef7fa2606682d82d672511de5524f1bc7ea
Igor A Tarasov <develop@dicr.org>
2021-04-09 07:59:35 +0500

    rename Token fields

--- src/entity/Token.php
+++ src/entity/Token.php
@@ -3,51 +3,49 @@
  * @copyright 2019-2021 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license MIT
- * @version 09.04.21 04:19:36
+ * @version 09.04.21 07:58:58
  */
 
 declare(strict_types = 1);
 namespace dicr\yandex\oauth\entity;
 
 use dicr\json\JsonEntity;
-use InvalidArgumentException;
 
 /**
  * Токен доступа.
  *
- * @property int $createTime timestamp создания токена
- * @property-read ?int $expireTime timestamp время создания
+ * @property-read ?int $expireTime timestamp окончания
  */
 class Token extends JsonEntity
 {
+    /** @var int timestamp времени создания */
+    public $created;
+
     /** @var string Тип выданного токена. Всегда принимает значение «bearer». */
-    public $tokenType;
+    public $token_type;
 
     /** @var string OAuth-токен с запрошенными правами или с правами, указанными при регистрации приложения. */
-    public $accessToken;
+    public $access_token;
 
     /** @var int Время жизни токена в секундах. */
-    public $expiresIn;
+    public $expires_in;
 
     /** @var string Токен, который можно использовать для продления срока жизни соответствующего OAuth-токена. */
-    public $refreshToken;
+    public $refresh_token;
 
     /**
-     * @var string[]|null
+     * @var string разделенные пробелом права доступа
      * Права, запрошенные разработчиком или указанные при регистрации приложения. Поле scope является дополнительным
      * и возвращается, если OAuth предоставил токен с меньшим набором прав, чем было запрошено.
      */
     public $scope;
-
-    /** @var int время создания */
-    private $_createTime;
 
     /**
      * @inheritDoc
      */
     public function __construct(array $config = [])
     {
-        $this->_createTime = time();
+        $this->created = time();
 
         parent::__construct($config);
     }
@@ -55,55 +53,10 @@ class Token extends JsonEntity
     /**
      * @inheritDoc
      */
-    public function attributes(): array
+    public function attributeFields(): array
     {
-        return array_merge(parent::attributes(), [
-            'createTime'
-        ]);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function attributesToJson(): array
-    {
-        return [
-            'scope' => static fn($val) => empty($val) ? null : implode(' ', (array)$val)
-        ];
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function attributesFromJson(): array
-    {
-        return [
-            'scope' => static fn($val) => empty($val) ? null : explode(' ', (string)$val)
-        ];
-    }
-
-    /**
-     * Время создания.
-     *
-     * @return int
-     */
-    public function getCreateTime(): int
-    {
-        return $this->_createTime;
-    }
-
-    /**
-     * Установить время создания.
-     *
-     * @param int $time
-     */
-    public function setCreateTime(int $time): void
-    {
-        if ($time <= 0) {
-            throw new InvalidArgumentException('time: ' . $time);
-        }
-
-        $this->_createTime = $time;
+        // для соответствия стандартному токену не конвертируем в camelCase
+        return [];
     }
 
     /**
@@ -113,6 +66,6 @@ class Token extends JsonEntity
      */
     public function getExpireTime(): ?int
     {
-        return empty($this->expiresIn) ? null : $this->_createTime + $this->expiresIn;
+        return empty($this->expires_in) ? null : $this->created + $this->expires_in;
     }
 }
